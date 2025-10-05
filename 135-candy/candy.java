fe23f52394a0276cc59eddce0f79098ca4a50f6c
Omjaiswal241 <jaiswalom221@gmail.com>
2025-10-05 23:32:47 +0530

    Time: 3 ms (80.37%) | Memory: 45.8 MB (57.75%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int candy(int[] ratings) {
-        int n=ratings.length,sum=0;
+    public int candy(int[] ar) {
+        int n=ar.length,sum=0;
         int [] left=new int[n];
         int [] right=new int[n];
         left[0]=1;
         for(int i=1;i<n;i++)
         {
-            if(ratings[i]>ratings[i-1])
+            if(ar[i]>ar[i-1])
             {
                 left[i]=left[i-1]+1;
             }
@@ -18,7 +18,7 @@ class Solution {
         right[n-1]=1;
         for(int i=n-2;i>=0;i--)
         {
-            if(ratings[i]>ratings[i+1])
+            if(ar[i]>ar[i+1])
             {
                 right[i]=right[i+1]+1;
             }
