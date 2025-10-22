1e3b852f36e1d81117232adf8a9ec6a4b66cfc90
Omjaiswal241 <jaiswalom221@gmail.com>
2025-10-22 20:25:16 +0530

    Time: 1 ms (26.20%) | Memory: 40.6 MB (53.93%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -1,36 +1,36 @@
 class Solution {
     public int maximumSwap(int num) {
-        String res=kswap(num+"",1);
-        return Integer.parseInt(res);
-    }
-    public static String kswap(String str,int k)
-    {
-        if(k==0)
+        String str=num+"";
+        int n=str.length();
+
+        char [] digit=new char[n];
+        for(int i=0;i<n;i++)
         {
-            return str;
+            digit[i]=str.charAt(i);
         }
-        String max=str;
-        for(int i=0;i<str.length()-1;i++)
+
+        int idx[]=new int[n];
+        idx[n-1]=n-1;
+        int max=n-1;
+        for(int i=n-2;i>=0;i--)
         {
-            for(int j=i+1;j<str.length();j++)
+            if(digit[i]>digit[max])
             {
-                String newstr=swap(str,i,j);
-                if(Integer.parseInt(newstr)>Integer.parseInt(max))
-                {
-                    max=newstr;
-                }
-                kswap(newstr,k-1);
+                max=i;
+            }
+            idx[i]=max;
+        }
+
+        for(int i=0;i<n-1;i++)
+        {
+            if(digit[i]!=digit[idx[i]])
+            {
+                char temp=digit[i];
+                digit[i]=digit[idx[i]];
+                digit[idx[i]]=temp;
+                return Integer.parseInt(new String(digit));
             }
         }
-        return max;
-    }
-    public static String swap(String str,int i,int j)
-    {
-        StringBuilder sb=new StringBuilder(str);
-        char ich=sb.charAt(i);
-        char jch=sb.charAt(j);
-        sb.setCharAt(i,jch);
-        sb.setCharAt(j,ich);
-        return sb.toString();
+        return num;
     }
 }
