91ef762f8de01a38434549a0e76d1df3897f4a7c
Om Jaiswal <jaiswalom221@gmail.com>
2026-01-16 23:05:13 +0530

    Time: 1 ms (59.36%) | Memory: 49 MB (5.74%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,32 +1,23 @@
 class Solution {
     public int trap(int[] height) {
         int n=height.length;
-       int premax=height[0];
-       int [] prefix=new int[n];
-       prefix[0]=height[0];
-       for(int i=1;i<n;i++)
-       {
-        premax=Math.max(premax,height[i]);
-        prefix[i]=premax;
-       }
-       int suffmax=height[n-1];
-       int [] suffix=new int[n];
-       suffix[n-1]=height[n-1];
-       for(int i=n-2;i>=0;i--)
-       {
-        suffmax=Math.max(suffmax,height[i]);
-        suffix[i]=suffmax;
-       }
-       int total=0;
-       for(int i=0;i<n;i++)
-       {
-        int leftmax=prefix[i];
-        int rightmax=suffix[i];
-        if(height[i]<leftmax && height[i]<rightmax)
+        int pre[]=new int[n];
+        int suff[]=new int[n];
+        pre[0]=height[0];
+        suff[n-1]=height[n-1];
+        for(int i=1;i<n;i++)
         {
-            total+=Math.min(leftmax,rightmax)-height[i];
+            pre[i]=Math.max(height[i],pre[i-1]);
         }
-       }
-       return total; 
+        for(int i=n-2;i>=0;i--)
+        {
+            suff[i]=Math.max(height[i],suff[i+1]);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            ans+=Math.min(pre[i],suff[i])-height[i];
+        }
+        return ans;
     }
 }
