c08bdcff9b278e91e766cf00433c63de72080bc1
Om Jaiswal <jaiswalom221@gmail.com>
2026-01-18 23:08:26 +0530

    Time: 142 ms (5.91%) | Memory: 44.7 MB (14.85%) - LeetSync

--- 312-burst-balloons/burst-balloons.java
+++ 312-burst-balloons/burst-balloons.java
@@ -1,21 +1,21 @@
 class Solution {
     public int maxCoins(int[] nums) {
-       int n=nums.length;
-       ArrayList<Integer> li=new ArrayList<>();
-       li.add(1);
-       for(int i=0;i<nums.length;i++)
-       {
-        li.add(nums[i]);
-       } 
-       li.add(1);
-       int dp[][]=new int[n+2][n+2];
-       for(int i[]:dp)
-       {
-        Arrays.fill(i,-1);
-       }
-       return helper(1,n,li,dp);
+        int n=nums.length;
+        ArrayList<Integer> li=new ArrayList<>();
+        li.add(1);
+        for(int i=0;i<n;i++)
+        {
+            li.add(nums[i]);
+        }
+        li.add(1);
+        int dp[][]=new int[n+2][n+2];
+        for(int i[]:dp)
+        {
+            Arrays.fill(i,-1);
+        }
+        return helper(li,1,n,dp);
     }
-    public static int helper(int i,int j,ArrayList<Integer> li,int dp[][])
+    public int helper(ArrayList<Integer> li,int i,int j,int [][]dp)
     {
         if(i>j)
         {
@@ -25,12 +25,12 @@ class Solution {
         {
             return dp[i][j];
         }
-        int max=Integer.MIN_VALUE;
+        int coins=Integer.MIN_VALUE;
         for(int idx=i;idx<=j;idx++)
         {
-            int cost=li.get(i-1)*li.get(idx)*li.get(j+1)+helper(i,idx-1,li,dp)+helper(idx+1,j,li,dp);
-            max=Math.max(max,cost);
+            int curr=li.get(i-1)*li.get(idx)*li.get(j+1)+helper(li,i,idx-1,dp)+helper(li,idx+1,j,dp);
+            coins=Math.max(coins,curr);
         }
-        return dp[i][j]=max;
+        return dp[i][j]=coins;
     }
 }
