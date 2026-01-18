6e224a4e72efc21b62b38c2716463989fde37b62
Om Jaiswal <jaiswalom221@gmail.com>
2026-01-18 10:41:24 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (10.13%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,13 +1,14 @@
 class Solution {
     public int rob(int[] nums) {
-       int oinc=nums[0],oexc=0;
-       for(int i=1;i<nums.length;i++)
-       {
-        int nexc=Math.max(oinc,oexc);
-        int ninc=nums[i]+oexc;
-        oexc=nexc;
-        oinc=ninc;
-       }
-       return Math.max(oexc,oinc); 
+        int oinc=nums[0];
+        int oexc=0;
+        for(int i=1;i<nums.length;i++)
+        {
+            int ninc=Math.max(oexc+nums[i],oinc);
+            int nexc=oinc;
+            oinc=ninc;
+            oexc=nexc;
+        }
+        return Math.max(oinc,oexc);
     }
 }
