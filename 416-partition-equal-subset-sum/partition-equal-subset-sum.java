f7aa90f7f89fd11eb0ecf070d297008ec3931554
Om Jaiswal <jaiswalom221@gmail.com>
2026-01-18 11:52:58 +0530

    Time: 94 ms (22.00%) | Memory: 76 MB (14.07%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int n=nums.length;
         int sum=0;
+        int n=nums.length;
         for(int i=0;i<n;i++)
         {
             sum+=nums[i];
@@ -11,43 +11,34 @@ class Solution {
             return false;
         }
         int target=sum/2;
-        if(target(nums,target))
+        int dp[][]=new int[n][target+1];
+        for(int i[]:dp)
+        {
+            Arrays.fill(i,-1);
+        }
+        return helper(nums,n-1,target,dp);
+    }
+    public boolean helper(int nums[],int idx,int target,int [][]dp)
+    {
+        if(target==0)
         {
             return true;
         }
-        return false;
-    }
-    public static boolean target(int nums[],int target)
-    {
-        boolean dp[][]=new boolean[nums.length+1][target+1];
-        for(int i=0;i<=nums.length;i++)
+        if(idx==0)
         {
-            for(int j=0;j<=target;j++)
-            {
-                if(i==0 && j==0)
-                {
-                    dp[i][j]=true;
-                }
-                else if(i==0)
-                {
-                    dp[i][j]=false;
-                }
-                else if(j==0)
-                {
-                    dp[i][j]=true;
-                }
-                else
-                {
-                    boolean inc=(j-nums[i-1]>=0)?dp[i-1][j-nums[i-1]]:false;
-                    boolean exc=dp[i-1][j];
-                    dp[i][j]=inc||exc;
-                }
-            }
-            if(dp[i][target]==true)
-            {
-                return true;
-            }
+            return nums[0]==target;
         }
-        return false;
+        if(dp[idx][target]!=-1)
+        {
+            return dp[idx][target]==1;
+        }
+        boolean nottake=helper(nums,idx-1,target,dp);
+        boolean take=false;
+        if(nums[idx]<=target)
+        {
+            take=helper(nums,idx-1,target-nums[idx],dp);
+        }
+        dp[idx][target]=(take||nottake)?1:0;
+        return take||nottake;
     }
 }
