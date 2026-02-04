9feee7db33d886ee1dc959e8d4610ef42c46df38
Om Jaiswal <jaiswalom221@gmail.com>
2026-02-04 15:39:58 +0530

    Time: 21 ms (25.65%) | Memory: 46.5 MB (31.68%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -2,67 +2,30 @@ class Solution {
     public int countSubstrings(String s) {
         int n=s.length();
         int count=0;
-        for(int i=0;i<n;i++)
+        boolean t[][]=new boolean [n][n];
+        for(int l=1;l<=n;l++)
         {
-            for(int j=i+1;j<=n;j++)
+            for(int i=0;i<n-l+1;i++)
             {
-                if(ispalin(s.substring(i,j)))
+                int j=i+l-1;
+                if(i==j)
+                {
+                    t[i][j]=true;
+                }
+                else if(i+1==j)
+                {
+                    t[i][j]=(s.charAt(i)==s.charAt(j));
+                }
+                else
+                {
+                    t[i][j]=(s.charAt(i)==s.charAt(j) && t[i+1][j-1]);
+                }
+                if(t[i][j]==true)
                 {
                     count++;
                 }
             }
         }
         return count;
-    }
-    // public static boolean ispalin(String s)
-    // {
-    //     String str=reverse(s);
-    //     int n=s.length();
-    //     int dp[][]=new int[n][n];
-    //     for(int i[]:dp)
-    //     {
-    //         Arrays.fill(i,-1);
-    //     }
-    //     int c=check(s,n-1,str,n-1,dp);
-    //     return (c==n);
-    // }
-    // public static int check(String s,int idx1,String t,int idx2,int [][]dp)
-    // {
-    //     if(idx1<0 || idx2<0)
-    //     {
-    //         return 0;
-    //     }
-    //     if(dp[idx1][idx2]!=-1)
-    //     {
-    //         return dp[idx1][idx2];
-    //     }
-    //     if(s.charAt(idx1)==t.charAt(idx2))
-    //     {
-    //         return dp[idx1][idx2]=1+check(s,idx1-1,t,idx2-1,dp);
-    //     }
-    //     return dp[idx1][idx2]=Math.max(check(s,idx1-1,t,idx2,dp),check(s,idx1,t,idx2-1,dp));
-    // }
-    // public static String reverse(String str)
-    // {
-    //     StringBuilder sb=new StringBuilder();
-    //     for(int i=str.length()-1;i>=0;i--)
-    //     {
-    //         sb.append(str.charAt(i));
-    //     }
-    //     return sb.toString();
-    // }
-    public static boolean ispalin(String s)
-    {
-        int start=0,end=s.length()-1;
-        while(start<end)
-        {
-            if(s.charAt(start)!=s.charAt(end))
-            {
-                return false;
-            }
-            start++;
-            end--;
-        }
-        return true;
     }
 }
