ae91ac67d03a80d4cde935180faf4b2cfe192c41
Om Jaiswal <jaiswalom221@gmail.com>
2026-02-19 19:30:53 +0530

    Time: 385 ms (68.43%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,6 +1,4 @@
-# Write your MySQL query statement below
 Select firstName,lastName,city,state
 from Person
-left join
-Address
-On Person.personId=Address.personId 
+left join Address
+On Person.personId=Address.personId
