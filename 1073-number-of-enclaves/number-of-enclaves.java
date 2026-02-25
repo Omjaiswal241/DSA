e781700921af5de048985448d44a19b7cadbd807
Om Jaiswal <jaiswalom221@gmail.com>
2026-02-25 23:42:58 +0530

    Time: 7 ms (84.59%) | Memory: 60 MB (58.95%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int count=0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 || j==0 || i==m-1 || j==n-1)
+                {
+                    if(grid[i][j]==1)
+                    {
+                        helper(grid,i,j);
+                    }
+                }
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void helper(int [][]grid,int i,int j)
+    {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0)
+        {
+            return;
+        } 
+        grid[i][j]=0;
+        helper(grid,i-1,j);
+        helper(grid,i,j-1);
+        helper(grid,i+1,j);
+        helper(grid,i,j+1);
+    }
+}
