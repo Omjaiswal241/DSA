e13ee56537a61bc82c7d05024faddb705781200a
Om Jaiswal <jaiswalom221@gmail.com>
2026-04-03 12:21:37 +0530

    Time: 3 ms (98.34%) | Memory: 46.7 MB (92.48%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,32 +1,32 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int n=word1.length();
-        int m=word2.length();
-        int dp[][]=new int[n][m];
+        int idx1=word1.length();
+        int idx2=word2.length();
+        int dp[][]=new int[idx1][idx2];
         for(int i[]:dp)
         {
             Arrays.fill(i,-1);
         }
-        return helper(word1,n-1,word2,m-1,dp);
+        return helper(word1,idx1-1,word2,idx2-1,dp);
     }
-    public static int helper(String s,int i,String t,int j,int [][]dp)
+    public int helper(String s,int idx1,String t,int idx2,int [][]dp)
     {
-        if(i<0)
+        if(idx2<0)
         {
-            return j+1;
+            return idx1+1;
         }
-        if(j<0)
+        if(idx1<0)
         {
-            return i+1;
+            return idx2+1;
         }
-        if(dp[i][j]!=-1)
+        if(dp[idx1][idx2]!=-1)
         {
-            return dp[i][j];
+            return dp[idx1][idx2];
         }
-        if(s.charAt(i)==t.charAt(j))
+        if(s.charAt(idx1)==t.charAt(idx2))
         {
-            return dp[i][j]=helper(s,i-1,t,j-1,dp);
+            return dp[idx1][idx2]=helper(s,idx1-1,t,idx2-1,dp);
         }
-        return dp[i][j]=Math.min(Math.min(1+helper(s,i-1,t,j-1,dp),1+helper(s,i-1,t,j,dp)),(1+helper(s,i,t,j-1,dp)));
+        return dp[idx1][idx2]=1+Math.min(Math.min(helper(s,idx1-1,t,idx2,dp),helper(s,idx1,t,idx2-1,dp)),helper(s,idx1-1,t,idx2-1,dp));
     }
 }
