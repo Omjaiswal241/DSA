d418d073755083087e2e1641ca1a3ddc37cd3dd6
Om Jaiswal <jaiswalom221@gmail.com>
2026-04-07 18:25:22 +0530

    Time: 19 ms (61.15%) | Memory: 63.1 MB (18.74%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -2,33 +2,34 @@ class Solution {
     public boolean isMatch(String s, String p) {
         int n=s.length();
         int m=p.length();
-        int dp[][]=new int [n][m];
+        int dp[][]=new int[n][m];
         for(int i[]:dp)
         {
             Arrays.fill(i,-1);
         }
-        return (helper(s,n-1,p,m-1,dp)==1);
+        return helper(s,n-1,p,m-1,dp)==1;
+
     }
-    public static int helper(String s,int i,String p,int j,int [][]dp)
+    public int helper(String s,int i,String p,int j,int dp[][])
     {
         if(i<0 && j<0)
         {
             return 1;
         }
-        if(i>=0 && j<0)
-        {
-            return 0;
-        }
         if(i<0 && j>=0)
         {
-            for(int f=j;f>=0;f--)
+            for(int k=j;k>=0;k--)
             {
-                if(p.charAt(f)!='*')
+                if(p.charAt(k)!='*')
                 {
                     return 0;
                 }
             }
             return 1;
+        }
+        if(i>=0 && j<0)
+        {
+            return 0;
         }
         if(dp[i][j]!=-1)
         {
@@ -38,9 +39,9 @@ class Solution {
         {
             return dp[i][j]=helper(s,i-1,p,j-1,dp);
         }
-        if(p.charAt(j)=='*')
+        else if(p.charAt(j)=='*')
         {
-        return dp[i][j]=(helper(s,i-1,p,j,dp)==1 || helper(s,i,p,j-1,dp)==1)?1:0;
+            return dp[i][j]=(helper(s,i-1,p,j,dp)==1 || helper(s,i,p,j-1,dp)==1)?1:0;
         }
         return dp[i][j]=0;
     }
