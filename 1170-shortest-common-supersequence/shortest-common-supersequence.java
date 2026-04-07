f9ddd5f8d52459f8d3b8fc56e3b2ecb50351f8b2
Om Jaiswal <jaiswalom221@gmail.com>
2026-04-07 10:29:36 +0530

    Time: 23 ms (72.41%) | Memory: 53.7 MB (76.57%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.java
@@ -3,13 +3,17 @@ class Solution {
         int n=str1.length();
         int m=str2.length();
         int dp[][]=new int[n+1][m+1];
-        for(int i=1;i<=n;i++)
+        for(int i=0;i<=n;i++)
         {
-            for(int j=1;j<=m;j++)
+            for(int j=0;j<=m;j++)
             {
-                if(str1.charAt(i-1)==str2.charAt(j-1))
+                if(i==0 || j==0)
                 {
-                    dp[i][j]=1+dp[i-1][j-1];
+                    dp[i][j]=0;
+                }
+                else if(str1.charAt(i-1)==str2.charAt(j-1))
+                {
+                    dp[i][j]=dp[i-1][j-1]+1;
                 }
                 else
                 {
@@ -17,47 +21,37 @@ class Solution {
                 }
             }
         }
-        String ans="";
+        StringBuilder sb=new StringBuilder();
         int i=n,j=m;
         while(i>0 && j>0)
         {
             if(str1.charAt(i-1)==str2.charAt(j-1))
             {
-                ans+=str1.charAt(i-1);
+                sb.append(str1.charAt(i-1));
                 i--;
                 j--;
             }
             else if(dp[i-1][j]>dp[i][j-1])
             {
-                ans+=str1.charAt(i-1);
+                sb.append(str1.charAt(i-1));
                 i--;
             }
             else
             {
-                ans+=str2.charAt(j-1);
+                sb.append(str2.charAt(j-1));
                 j--;
             }
         }
         while(i>0)
         {
-            ans+=str1.charAt(i-1);
+            sb.append(str1.charAt(i-1));
             i--;
         }
         while(j>0)
         {
-            ans+=str2.charAt(j-1);
+            sb.append(str2.charAt(j-1));
             j--;
         }
-        ans=reverse(ans);
-        return ans;
-    }
-    public String reverse(String str)
-    {
-        String s="";
-        for(int i=str.length()-1;i>=0;i--)
-        {
-            s+=str.charAt(i);
-        }
-        return s;
+        return sb.reverse().toString();
     }
 }
