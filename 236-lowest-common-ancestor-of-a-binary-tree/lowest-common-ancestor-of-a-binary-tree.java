7708465dcb7e913f23d61841c04c80a2989f1918
Om Jaiswal <jaiswalom221@gmail.com>
2026-04-10 10:22:03 +0530

    Time: 9 ms (13.23%) | Memory: 47.8 MB (5.16%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -9,23 +9,50 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root==null || root==p || root==q)
+        ArrayList<TreeNode> a=nodetoroot(root,p);
+        ArrayList<TreeNode> b=nodetoroot(root,q);
+        int x=a.size()-1;
+        int y=b.size()-1;
+        while(x>=0 && y>=0)
         {
-            return root;
+            if(a.get(x).val==b.get(y).val)
+            {
+                x--;
+                y--;
+            }
+            else
+            {
+                break;
+            }
         }
-        TreeNode left=lowestCommonAncestor(root.left,p,q);
-        TreeNode right=lowestCommonAncestor(root.right,p,q);
-        if(left==null)
+        x++;
+        y++;
+        return a.get(x);
+    }
+    public ArrayList<TreeNode> nodetoroot(TreeNode root,TreeNode tar)
+    {
+        if(root==null)
         {
-            return right;
+            return new ArrayList<>();
         }
-        else if(right==null)
+        if(root==tar)
         {
+            ArrayList<TreeNode> base=new ArrayList<>();
+            base.add(root);
+            return base;
+        }
+        ArrayList<TreeNode> left=nodetoroot(root.left,tar);
+        if(left.size()!=0)
+        {
+            left.add(root);
             return left;
         }
-        else
+        ArrayList<TreeNode> right=nodetoroot(root.right,tar);
+        if(right.size()!=0)
         {
-            return root;
+            right.add(root);
+            return right;
         }
+        return new ArrayList<>();
     }
 }
