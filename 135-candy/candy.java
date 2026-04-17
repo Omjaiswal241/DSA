c3e2656e57cc7f0be5e86a50a7394abbc701453b
Om Jaiswal <jaiswalom221@gmail.com>
2026-04-17 18:52:45 +0530

    Time: 3 ms (87.06%) | Memory: 48 MB (8.85%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,24 +1,24 @@
 class Solution {
     public int candy(int[] ratings) {
-        int sum=1;
         int i=1;
+        int sum=1;
         int n=ratings.length;
         while(i<n)
         {
-            if(ratings[i]==ratings[i-1])
+            if(i<n && ratings[i]==ratings[i-1])
             {
                 sum+=1;
                 i++;
             }
             int peak=1;
-            while(i<n && ratings[i]>ratings[i-1])
+            while(i<n && ratings[i-1]<ratings[i])
             {
                 peak++;
                 sum+=peak;
                 i++;
             }
             int down=1;
-            while(i<n && ratings[i]<ratings[i-1])
+            while(i<n && ratings[i-1]>ratings[i])
             {
                 sum+=down;
                 down++;
