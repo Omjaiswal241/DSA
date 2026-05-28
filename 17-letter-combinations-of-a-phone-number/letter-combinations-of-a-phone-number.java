2528511e408b880ba558d695de1ade69183e36b7
Om Jaiswal <jaiswalom221@gmail.com>
2026-05-28 15:27:05 +0530

    Time: 3 ms (33.86%) | Memory: 48.9 MB (22.96%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,30 +1,23 @@
 class Solution {
-    String keypad[]={"@","#","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    public List<String> letterCombinations(String digits) {
+    String temp[]={"#","@","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digits) {   
         if(digits.length()==0)
         {
-            return new ArrayList<>();
+            List<String> base=new ArrayList<>();
+            base.add("");
+            return base; 
         }
-        return helper(digits,0,digits.length());
-    }
-    public List<String> helper(String s,int idx,int d) 
-    {
-        if(idx==d)
-        {
-            List<String> li=new ArrayList<>();
-            li.add("");
-            return li;
-        }
-        char a=s.charAt(0);
-        String roq=s.substring(1);
-        List<String> temp=helper(roq,idx+1,d);
+        char x=digits.charAt(0);
+        String roq=digits.substring(1);
+        List<String> f=letterCombinations(roq);
+        String y=temp[Integer.parseInt(x+"")];
         List<String> res=new ArrayList<>();
-        String f=keypad[Integer.parseInt(a+"")];
-        for(int i=0;i<f.length();i++)
+        for(int i=0;i<y.length();i++)
         {
-            for(int j=0;j<temp.size();j++)
+            char g=y.charAt(i);
+            for(int j=0;j<f.size();j++)
             {
-                res.add(f.charAt(i)+temp.get(j));
+                res.add(g+f.get(j));
             }
         }
         return res;
