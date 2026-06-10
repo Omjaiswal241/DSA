d3d0aa4615b2b24c85416d3837e568c979af14f8
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-11 00:11:09 +0530

    Time: 20 ms (88.07%) | Memory: 60.4 MB (59.09%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -8,8 +8,8 @@ class Solution {
         }
         int a=(n>4)?nums[n-4]-nums[0]:Integer.MAX_VALUE;
         int b=(n>3)?nums[n-1]-nums[3]:Integer.MAX_VALUE;
-        int c=(n>2)?nums[n-3]-nums[1]:0;
-        int d=(n>1)?nums[n-2]-nums[2]:0;
+        int c=(n>2)?nums[n-3]-nums[1]:Integer.MAX_VALUE;
+        int d=(n>1)?nums[n-2]-nums[2]:Integer.MAX_VALUE;
         return Math.min(a,Math.min(b,Math.min(c,d)));
     }
 }
