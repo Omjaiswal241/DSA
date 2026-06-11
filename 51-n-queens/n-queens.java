c6c1ad2ae09e3f1f5147344d918ad2feafcf8ca1
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-12 00:39:37 +0530

    Time: 3 ms (41.76%) | Memory: 46.5 MB (63.89%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,71 +1,65 @@
 class Solution {
+    List<List<String>> res=new ArrayList<>();
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> li=new ArrayList<List<String>>();
-        char [][] chess=new char[n][n];
-        nQueens(chess,0,li);
-        return li;
+        helper(new char[n][n],0);
+        return res;
     }
-    public static void nQueens(char [][]chess,int row,List<List<String>> li)
+    public void helper(char ar[][],int r)
     {
-        if(row==chess.length)
+        if(r==ar.length)
         {
-            savestr(chess,li);
-            return;
-        }
-        for(int col=0;col<chess[0].length;col++)
-        {
-            if(issafe(chess,row,col))
+            List<String> li=new ArrayList<>();
+            for(int i=0;i<ar.length;i++)
             {
-                chess[row][col]='Q';
-                nQueens(chess,row+1,li);
-                chess[row][col]='.';
+                StringBuilder sb=new StringBuilder();
+                for(int j=0;j<ar[0].length;j++)
+                {
+                    if(ar[i][j]!='Q')
+                    {
+                        sb.append('.');
+                    }
+                    else
+                    {
+                        sb.append('Q');
+                    }
+                }
+                li.add(sb.toString());
+            }
+            res.add(new ArrayList<>(li));
+        }
+        for(int c=0;c<ar[0].length;c++)
+        {
+            if(issafe(ar,r,c))
+            {
+                ar[r][c]='Q';
+                helper(ar,r+1);
+                ar[r][c]='.';
             }
         }
     }
-    public static boolean issafe(char [][] chess, int r, int c)
+    public boolean issafe(char ar[][],int r,int c)
     {
         for(int i=r-1,j=c;i>=0;i--)
         {
-            if(chess[i][j]=='Q')
+            if(ar[i][j]=='Q')
             {
                 return false;
             }
         }
         for(int i=r-1,j=c-1;i>=0 && j>=0;i--,j--)
         {
-            if(chess[i][j]=='Q')
+            if(ar[i][j]=='Q')
             {
                 return false;
             }
         }
-        for(int i=r-1,j=c+1;i>=0 && j<chess[0].length;i--,j++)
+        for(int i=r-1,j=c+1;i>=0 && j<ar[0].length;i--,j++)
         {
-            if(chess[i][j]=='Q')
+            if(ar[i][j]=='Q')
             {
                 return false;
             }
         }
         return true;
-    }
-    public static void savestr(char [][]chess,List<List<String>> li)
-    {
-        List<String> temp=new ArrayList<>();
-        for(int i=0;i<chess.length;i++)
-        {
-            String row="";
-            for(int j=0;j<chess[0].length;j++)
-            {
-                if(chess[i][j]=='Q')
-                {
-                    row+='Q';
-                }
-                else
-                {
-                    row+='.';
-                }
-            }
-            temp.add(row);
-        }
-        li.add(temp);
     }
 }
