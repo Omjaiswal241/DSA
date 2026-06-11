ae13d4eb8b42e29900a0ac80923d0e45c9e4ec28
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-11 18:19:33 +0530

    Time: 3 ms (34.10%) | Memory: 49.3 MB (7.20%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,23 +1,23 @@
 class Solution {
-    String temp[]={"#","@","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    public List<String> letterCombinations(String digits) {   
+    String ar[]={"@","#","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    public List<String> letterCombinations(String digits) {
         if(digits.length()==0)
         {
             List<String> base=new ArrayList<>();
             base.add("");
-            return base; 
-        }
-        char x=digits.charAt(0);
+            return base;
+        }   
+        char ch=digits.charAt(0);
         String roq=digits.substring(1);
-        List<String> f=letterCombinations(roq);
-        String y=temp[Integer.parseInt(x+"")];
+        List<String> temp=letterCombinations(roq);
+        String f=ar[Integer.parseInt(ch+"")];
         List<String> res=new ArrayList<>();
-        for(int i=0;i<y.length();i++)
+        for(int i=0;i<f.length();i++)
         {
-            char g=y.charAt(i);
-            for(int j=0;j<f.size();j++)
+            char x=f.charAt(i);
+            for(int j=0;j<temp.size();j++)
             {
-                res.add(g+f.get(j));
+                res.add(x+temp.get(j)+"");
             }
         }
         return res;
