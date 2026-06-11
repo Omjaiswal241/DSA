ef33c488a7edf7406a00ea38cced058bc0327b2c
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-12 00:04:41 +0530

    Time: 21 ms (59.73%) | Memory: 63.5 MB (12.54%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,48 +1,47 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        int n=s.length();
-        int m=p.length();
-        int dp[][]=new int[n][m];
+        int idx1=s.length()-1;
+        int idx2=p.length()-1;
+        int dp[][]=new int[idx1+1][idx2+1];
         for(int i[]:dp)
         {
             Arrays.fill(i,-1);
         }
-        return helper(s,n-1,p,m-1,dp)==1;
-
+        return helper(s,idx1,p,idx2,dp)==1;
     }
-    public int helper(String s,int i,String p,int j,int dp[][])
+    public int helper(String s,int idx1,String p,int idx2,int dp[][])
     {
-        if(i<0 && j<0)
+        if(idx1<0 && idx2<0)
         {
             return 1;
         }
-        if(i<0 && j>=0)
+        if(idx1<0)
         {
-            for(int k=j;k>=0;k--)
+            for(int i=idx2;i>=0;i--)
             {
-                if(p.charAt(k)!='*')
+                if(p.charAt(i)!='*')
                 {
                     return 0;
                 }
             }
             return 1;
         }
-        if(i>=0 && j<0)
+        if(idx2<0 && idx1>=0)
         {
             return 0;
         }
-        if(dp[i][j]!=-1)
+        if(dp[idx1][idx2]!=-1)
         {
-            return dp[i][j];
+            return dp[idx1][idx2];
         }
-        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?')
+        if(s.charAt(idx1)==p.charAt(idx2) || p.charAt(idx2)=='?')
         {
-            return dp[i][j]=helper(s,i-1,p,j-1,dp);
+            return dp[idx1][idx2]=helper(s,idx1-1,p,idx2-1,dp);
         }
-        else if(p.charAt(j)=='*')
+        else if(p.charAt(idx2)=='*')
         {
-            return dp[i][j]=(helper(s,i-1,p,j,dp)==1 || helper(s,i,p,j-1,dp)==1)?1:0;
+            return dp[idx1][idx2]=(helper(s,idx1-1,p,idx2,dp)==1 || helper(s,idx1,p,idx2-1,dp)==1)?1:0;
         }
-        return dp[i][j]=0;
+        return dp[idx1][idx2]=0;
     }
 }
