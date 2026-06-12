fe760135a9927633f7fdf50445680eef7a920945
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-13 02:40:17 +0530

    Time: 2261 ms (32.70%) | Memory: 43 MB (69.63%) - LeetSync

--- 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int minCut(String s) {
-        int n=s.length();
-        int dp[]=new int[n];
-        Arrays.fill(dp,-1);
-        return helper(s,0,dp)-1;
+       int n=s.length();
+       int dp[]=new int[n];
+       Arrays.fill(dp,-1);
+       return helper(s,0,dp)-1; 
     }
-    public int helper(String s,int idx,int dp[])
+    public int helper(String s,int idx,int []dp)
     {
         if(idx==s.length())
         {
@@ -21,23 +21,22 @@ class Solution {
             if(ispalin(s,idx,i))
             {
                 int cost=1+helper(s,i+1,dp);
-                min_cost=Math.min(cost,min_cost);
+                min_cost=Math.min(min_cost,cost);
             }
         }
         return dp[idx]=min_cost;
     }
-    public boolean ispalin(String s,int i,int j)
+    public boolean ispalin(String s,int idx,int i)
     {
-        int low=i;
-        int high=j;
-        while(low<high)
+        int l=idx,h=i;
+        while(l<h)
         {
-            if(s.charAt(low)!=s.charAt(high))
+            if(s.charAt(l)!=s.charAt(h))
             {
                 return false;
             }
-            low++;
-            high--;
+            l++;
+            h--;
         }
         return true;
     }
