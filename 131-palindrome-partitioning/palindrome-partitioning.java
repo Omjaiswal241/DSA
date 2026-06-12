ebef4751932f3c548756b6c0bb056cc22b1edfe1
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-13 01:38:29 +0530

    Time: 10 ms (15.06%) | Memory: 65.5 MB (22.76%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,37 +1,37 @@
 class Solution {
+    List<List<String>> res=new ArrayList<>();
     public List<List<String>> partition(String s) {
-        List<List<String>> res=new ArrayList<>();
-        helper(0,s,new ArrayList<String>(),res);
+        helper(s,0,new ArrayList<>());
         return res;
     }
-    public void helper(int idx,String s,List<String> curr,List<List<String>> res)
+    public void helper(String s,int idx,List<String> li)
     {
         if(idx==s.length())
         {
-            res.add(new ArrayList<>(curr));
+            res.add(new ArrayList<>(li));
             return;
         }
         for(int i=idx;i<s.length();i++)
         {
             if(ispalin(s.substring(idx,i+1)))
             {
-                curr.add(s.substring(idx,i+1));
-                helper(i+1,s,curr,res);
-                curr.remove(curr.size()-1);
+                li.add(s.substring(idx,i+1));
+                helper(s,i+1,li);
+                li.remove(li.size()-1);
             }
         }
     }
     public boolean ispalin(String s)
     {
-        int left=0,right=s.length()-1;
-        while(left<right)
+        int l=0,h=s.length()-1;
+        while(l<h)
         {
-            if(s.charAt(left)!=s.charAt(right))
+            if(s.charAt(l)!=s.charAt(h))
             {
                 return false;
             }
-            left++;
-            right--;
+            l++;
+            h--;
         }
         return true;
     }
