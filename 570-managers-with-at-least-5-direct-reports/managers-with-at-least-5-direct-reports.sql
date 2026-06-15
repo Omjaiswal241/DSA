3419f949e0bd37aa3251456e6e7fc7551916949a
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-15 19:15:43 +0530

    Time: 376 ms (31.43%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-Select e1.name
+select e1.name 
 from Employee e1
-inner join Employee e2
+Inner JOin Employee e2
 On e1.id=e2.managerId
-Group BY e2.managerId
+Group by e2.managerId
 Having Count(e2.managerId)>=5
