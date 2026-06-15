b9108163fe677940a0072ec3def11301f670c035
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-15 18:16:27 +0530

    Time: 551 ms (18.75%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,6 @@
--- Write your PostgreSQL query statement below
-Select w1.id
-From Weather w1,Weather w2
-Where (w1.recordDate-w2.recordDate)=1 AND w1.temperature>w2.temperature;
+# Write your MySQL query statement below
+select w2.id as Id from Weather w1
+Inner Join
+Weather w2
+ON DATEDIFF(w2.recordDate,w1.recordDate)=1 
+Where w2.temperature>w1.temperature
