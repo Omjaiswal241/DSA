ed62deeb92a70e757fe64ac3ef4574e1d708740c
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-15 11:21:28 +0530

    Time: 142 ms (56.38%) | Memory: 47.2 MB (38.27%) - LeetSync

--- 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
+++ 1145-number-of-submatrices-that-sum-to-target/number-of-submatrices-that-sum-to-target.java
@@ -1,15 +1,15 @@
 class Solution {
     public int numSubmatrixSumTarget(int[][] matrix, int target) {
-        int res=0;
         int n=matrix.length;
         int m=matrix[0].length;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                matrix[i][j]=(j>0)?matrix[i][j]+matrix[i][j-1]:matrix[i][j];
+                matrix[i][j]=(j>0)?matrix[i][j-1]+matrix[i][j]:matrix[i][j];
             }
         }
+        int cnt=0;
         for(int sc=0;sc<m;sc++)
         {
             for(int ec=sc;ec<m;ec++)
@@ -20,14 +20,15 @@ class Solution {
                 for(int r=0;r<n;r++)
                 {
                     csum+=(sc>0)?matrix[r][ec]-matrix[r][sc-1]:matrix[r][ec];
-                    if(hm.containsKey(csum-target))
+                    int rem=csum-target;
+                    if(hm.containsKey(rem))
                     {
-                        res+=hm.get(csum-target);
+                        cnt+=hm.get(rem);
                     }
                     hm.put(csum,hm.getOrDefault(csum,0)+1);
                 }
             }
         }
-        return res;
+        return cnt;
     }
 }
