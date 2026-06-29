e8578bf3dd7594c68117a2b85f54076e9925020f
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-29 12:55:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (64.68%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,37 +1,53 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low=0;
-        int high=nums.length-1;
-        while(low<=high)
+        int n=nums.length;
+        int pivot_idx=pivot(nums,n);
+        int idx;
+        idx=binarysearch(nums,target,0,pivot_idx-1);
+        if(idx!=-1)
         {
-            int mid=low+(high-low)/2;
-            if(nums[mid]==target)
+            return idx; 
+        }
+        idx=binarysearch(nums,target,pivot_idx,n-1);
+        return idx;
+    }
+    public int pivot(int nums[],int n)
+    {
+        int l=0,h=n-1;
+        while(l<h)
+        {
+            int mid=l+(h-l)/2;
+            if(nums[mid]>nums[h])
             {
-                return mid;
-            }
-            if(nums[low]<=nums[mid])
-            {
-                if(nums[low]<=target && target<=nums[mid])
-                {
-                    high=mid-1;
-                }
-                else
-                {
-                    low=mid+1;
-                }
+                l=mid+1;
             }
             else
             {
-                if(nums[mid]<=target && target<=nums[high])
-                {
-                    low=mid+1;
-                }
-                else
-                {
-                    high=mid-1;
-                }
+                h=mid;
             }
         }
-        return -1;
+        return l;
+    }
+    public int binarysearch(int nums[],int target,int l,int h)
+    {
+        int ans=-1;
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                break;
+            }
+            else if(nums[mid]>target)
+            {
+                h=mid-1;
+            }
+            else
+            {
+                l=mid+1;
+            }
+        }
+        return ans;
     }
 }
