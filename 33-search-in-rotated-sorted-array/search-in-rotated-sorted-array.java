34e2cf164cbc23271eefccd0c90cbbea651dbca3
Om Jaiswal <jaiswalom221@gmail.com>
2026-06-30 11:44:21 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (11.06%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,53 +1,37 @@
 class Solution {
     public int search(int[] nums, int target) {
         int n=nums.length;
-        int pivot_idx=pivot(nums,n);
-        int idx;
-        idx=binarysearch(nums,target,0,pivot_idx-1);
-        if(idx!=-1)
-        {
-            return idx; 
-        }
-        idx=binarysearch(nums,target,pivot_idx,n-1);
-        return idx;
-    }
-    public int pivot(int nums[],int n)
-    {
         int l=0,h=n-1;
-        while(l<h)
-        {
-            int mid=l+(h-l)/2;
-            if(nums[mid]>nums[h])
-            {
-                l=mid+1;
-            }
-            else
-            {
-                h=mid;
-            }
-        }
-        return l;
-    }
-    public int binarysearch(int nums[],int target,int l,int h)
-    {
-        int ans=-1;
         while(l<=h)
         {
             int mid=l+(h-l)/2;
             if(nums[mid]==target)
             {
-                ans=mid;
-                break;
+                return mid;
             }
-            else if(nums[mid]>target)
+            if(nums[l]<=nums[mid])
             {
-                h=mid-1;
+                if(nums[l]<=target && target<=nums[mid])
+                {
+                    h=mid-1;
+                }
+                else
+                {
+                    l=mid+1;
+                }
             }
             else
             {
-                l=mid+1;
+                if(nums[mid]<=target && target<=nums[h])
+                {
+                    l=mid+1;
+                }
+                else
+                {
+                    h=mid-1;
+                }
             }
         }
-        return ans;
+        return -1;
     }
 }
