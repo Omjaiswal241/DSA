f1c8040ae226a86479a142827fa5356148433b5f
Om Jaiswal <jaiswalom221@gmail.com>
2026-07-10 12:59:29 +0530

    Time: 1 ms (96.58%) | Memory: 42.7 MB (36.65%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,13 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if(n<=0)
-        {
-            return false;
-        }
-        if((n&(n-1))==0)
-        {
-            return true;
-        }
-        return false;
+        return n>0 && Integer.bitCount(n)==1;
     }
 }
