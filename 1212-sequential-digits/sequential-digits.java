9d5136603e774b5327de6c2c50547c67d40b38e7
Om Jaiswal <jaiswalom221@gmail.com>
2026-07-13 18:37:43 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (22.13%) - LeetSync

--- 1212-sequential-digits/sequential-digits.java
+++ 1212-sequential-digits/sequential-digits.java
@@ -1,27 +1,26 @@
 class Solution {
     public List<Integer> sequentialDigits(int low, int high) {
-        Queue<Integer> qu=new ArrayDeque<>();
-        for(int i=1;i<=9;i++)
-        {
-            qu.add(i);
-        }
+        List<Integer> li=List.of(12,23,34,45,56,67,78,89,
+        123,234,345,456,567,678,789,
+        1234,2345,3456,4567,5678,6789,
+        12345,23456,34567,45678,56789,
+        123456,234567,345678,456789,
+        1234567,2345678,3456789,
+        12345678,23456789,
+        123456789);
         List<Integer> res=new ArrayList<>();
-        while(qu.size()!=0)
+        for(int i=0;i<li.size();i++)
         {
-            int a=qu.poll();
-            if(a>=low && a<=high)
+            int num=li.get(i);
+            if(num<low)
             {
-                res.add(a);
+                continue;
             }
-            int last_dig=a%10;
-            if(last_dig<9)
+            else if(num>high)
             {
-                int num=a*10+(last_dig+1);
-                if(num<=high)
-                {
-                    qu.add(num);
-                }
+                break;
             }
+            res.add(num);
         }
         return res;
     }
