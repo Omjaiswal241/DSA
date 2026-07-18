cf53911fc4a32eaf676dd809146b1096a929f6c4
Om Jaiswal <jaiswalom221@gmail.com>
2026-07-18 23:31:59 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (7.55%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -19,6 +19,8 @@ class Solution {
         {
             return 0;
         }
-        return Math.max(maxDepth(root.left),maxDepth(root.right))+1;
+        int left=maxDepth(root.left);
+        int right=maxDepth(root.right);
+        return Math.max(left,right)+1;
     }
 }
