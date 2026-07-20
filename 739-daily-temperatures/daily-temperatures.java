31b4844601c2e0f55f0882db4e73806a30bcc326
Om Jaiswal <jaiswalom221@gmail.com>
2026-07-20 20:02:52 +0530

    Time: 77 ms (27.14%) | Memory: 106.5 MB (73.46%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.java
+++ 739-daily-temperatures/daily-temperatures.java
@@ -1,43 +1,28 @@
 class Solution {
-    class Pair
-    {
-        int val,idx;
-        Pair(int val,int idx)
-        {
-            this.val=val;
-            this.idx=idx;
-        }
-    }
     public int[] dailyTemperatures(int[] temperatures) {
         int n=temperatures.length;
-        int ans[]=new int[n];
-        Stack<Pair> st=new Stack<>();
-        st.push(new Pair(temperatures[n-1],n-1));
-        for(int i=n-2;i>=0;i--)
+        Stack<Integer> st=new Stack<>();
+        st.push(n-1);
+        int res[]=new int[n];
+        res[n-1]=0;
+        int i=n-2;
+        while(i>=0)
         {
-            Pair top=new Pair(temperatures[i],i);
-            if(st.peek().val>top.val)
+            while(st.size()!=0 && temperatures[st.peek()]<=temperatures[i])
             {
-                ans[i]=st.peek().idx-i;
-                st.push(top);
+                st.pop();
+            }
+            if(st.size()==0)
+            {
+                res[i]=0;
             }
             else
             {
-                while(st.size()!=0 && st.peek().val<=top.val)
-                {
-                    st.pop();
-                }
-                if(st.size()==0)
-                {
-                    ans[i]=0;
-                }
-                else
-                {
-                ans[i]=st.peek().idx-i;
-                }
-                st.push(top);
+                res[i]=st.peek()-i;
             }
+            st.push(i);
+            i--;
         }
-        return ans;
+        return res;
     }
 }
