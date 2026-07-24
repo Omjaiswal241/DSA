56c021961ff44d280da349fc093dd3d2de1a33a9
Om Jaiswal <jaiswalom221@gmail.com>
2026-07-24 23:45:26 +0530

    Time: 429 ms (5.09%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,9 +1,9 @@
--- Write your PostgreSQL query statement below
-Select a1.machine_id,round(AVG(a2.timestamp-a1.timestamp)::numeric,3) AS processing_time
-From Activity a1
-join Activity a2
-ON a1.machine_id=a2.machine_id
-And a1.process_id=a2.process_id
-and a1.activity_type='start'
-and a2.activity_type='end'
-GROUP By a1.machine_id;
+# Write your MySQL query statement below
+select a1.machine_id,Round((AVG(a2.timestamp-a1.timestamp)),3) as processing_time
+from Activity a1
+Cross Join Activity a2
+On a1.machine_id=a2.machine_id
+AND a1.process_id=a2.process_id
+AND a2.activity_type='end'
+AND a1.activity_type='start'
+Group By a1.machine_id
