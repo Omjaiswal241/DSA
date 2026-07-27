4e25fc02ffd64231bfc73f494e2367a7db5ccfaf
Om Jaiswal <jaiswalom221@gmail.com>
2026-07-27 18:01:00 +0530

    Time: 8 ms (91.68%) | Memory: 49.3 MB (27.53%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,31 +1,28 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if(intervals.length==1)
-        {
-            return intervals;
-        }
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        List<int[]> res=new ArrayList<>();
-        res.add(intervals[0]);
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int[]> li=new ArrayList<>();
+        int n=intervals.length;
+        li.add(intervals[0]);
         int j=0;
         for(int i=1;i<intervals.length;i++)
         {
-            int interval[]=res.get(j);
-            if(intervals[i][0]<=interval[1])
+            int interval[]=li.get(j);
+            if(interval[1]>=intervals[i][0])
             {
-                interval[1]=Math.max(intervals[i][1],interval[1]);
+                interval[1]=Math.max(interval[1],intervals[i][1]);
             }
             else
             {
-                res.add(intervals[i]);
+                li.add(intervals[i]);
                 j++;
             }
         }
-        int ar[][]=new int[j+1][2];
-        for(int i=0;i<=j;i++)
+        int res[][]=new int[li.size()][2];
+        for(int i=0;i<li.size();i++)
         {
-            ar[i]=res.get(i);
-        }
-        return ar;
+            res[i]=li.get(i);
+        } 
+        return res;
     }
 }
