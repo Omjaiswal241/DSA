111a351608a3f6efd6e325031203428c1b3fb656
Om Jaiswal <jaiswalom221@gmail.com>
2026-08-01 00:19:56 +0530

    Time: 7 ms (83.33%) | Memory: 65.1 MB (7.51%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,43 +1,60 @@
 class Solution {
+    int M;
+    int N;
     public int numEnclaves(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        int count=0;
+        M=m;
+        N=n;
+        int temp[][]=new int[m][n];
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0 || j==0 || i==m-1 || j==n-1)
-                {
-                    if(grid[i][j]==1)
-                    {
-                        helper(grid,i,j);
-                    }
-                }
-            }
+            temp[i]=grid[i].clone();
         }
-        for(int i=0;i<m;i++)
+        boolean [][]visited=new boolean[m][n];
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            if(grid[0][i]==1)
             {
-                if(grid[i][j]==1)
-                {
-                    count++;
-                }
+                dfs(grid,0,i,temp,visited);
+            }
+            if(grid[m-1][i]==1)
+            {
+                dfs(grid,m-1,i,temp,visited);
             }
         }
-        return count;
+        for(int i=1;i<m-1;i++)
+        {
+            if(grid[i][0]==1)
+            {
+                dfs(grid,i,0,temp,visited);
+            }
+            if(grid[i][n-1]==1)
+            {
+                dfs(grid,i,n-1,temp,visited);
+            }
+        }
+        int res=0;
+        for(int i=1;i<m-1;i++)
+        {
+            for(int j=1;j<n-1;j++)
+            {
+                res+=temp[i][j];
+            }
+        }
+        return res;
     }
-    public static void helper(int [][]grid,int i,int j)
+    public void dfs(int grid[][],int i,int j,int[][] temp,boolean [][]visited)
     {
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0)
+        if(i<0 || j<0 || i>=M || j>=N || visited[i][j] || grid[i][j]==0)
         {
             return;
-        } 
-        grid[i][j]=0;
-        helper(grid,i-1,j);
-        helper(grid,i,j-1);
-        helper(grid,i+1,j);
-        helper(grid,i,j+1);
+        }
+        temp[i][j]=0;
+        visited[i][j]=true;
+        dfs(grid,i-1,j,temp,visited);
+        dfs(grid,i,j-1,temp,visited);
+        dfs(grid,i+1,j,temp,visited);
+        dfs(grid,i,j+1,temp,visited);
     }
 }
