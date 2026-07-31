099af3a7068ed56492beb50e0d8924f12ae41a0b
Om Jaiswal <jaiswalom221@gmail.com>
2026-08-01 00:40:54 +0530

    Time: 5 ms (99.90%) | Memory: 60.6 MB (51.35%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,37 +1,27 @@
 class Solution {
-    int M;
-    int N;
     public int numEnclaves(int[][] grid) {
         int m=grid.length;
         int n=grid[0].length;
-        M=m;
-        N=n;
-        int temp[][]=new int[m][n];
-        for(int i=0;i<m;i++)
-        {
-            temp[i]=grid[i].clone();
-        }
-        boolean [][]visited=new boolean[m][n];
         for(int i=0;i<n;i++)
         {
             if(grid[0][i]==1)
             {
-                dfs(grid,0,i,temp,visited);
+                dfs(grid,0,i);
             }
             if(grid[m-1][i]==1)
             {
-                dfs(grid,m-1,i,temp,visited);
+                dfs(grid,m-1,i);
             }
         }
         for(int i=1;i<m-1;i++)
         {
             if(grid[i][0]==1)
             {
-                dfs(grid,i,0,temp,visited);
+                dfs(grid,i,0);
             }
             if(grid[i][n-1]==1)
             {
-                dfs(grid,i,n-1,temp,visited);
+                dfs(grid,i,n-1);
             }
         }
         int res=0;
@@ -39,22 +29,21 @@ class Solution {
         {
             for(int j=1;j<n-1;j++)
             {
-                res+=temp[i][j];
+                res+=grid[i][j];
             }
         }
         return res;
     }
-    public void dfs(int grid[][],int i,int j,int[][] temp,boolean [][]visited)
+    public void dfs(int grid[][],int i,int j)
     {
-        if(i<0 || j<0 || i>=M || j>=N || visited[i][j] || grid[i][j]==0)
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0)
         {
             return;
         }
-        temp[i][j]=0;
-        visited[i][j]=true;
-        dfs(grid,i-1,j,temp,visited);
-        dfs(grid,i,j-1,temp,visited);
-        dfs(grid,i+1,j,temp,visited);
-        dfs(grid,i,j+1,temp,visited);
+        grid[i][j]=0;
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
     }
 }
