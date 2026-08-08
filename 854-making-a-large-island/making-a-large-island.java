85e10d8ad55c56077b18371d5939db928f5c1cd8
Om Jaiswal <jaiswalom221@gmail.com>
2026-08-08 23:09:53 +0530

    Time: 95 ms (64.49%) | Memory: 145.4 MB (59.24%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.java
+++ 854-making-a-large-island/making-a-large-island.java
@@ -1,8 +1,7 @@
 class Solution {
     public int largestIsland(int[][] grid) {
         int n=grid.length;
-        int mark=1;
-        int res=Integer.MIN_VALUE;
+        int mark=2;
         HashMap<Integer,Integer> hm=new HashMap<>();
         for(int i=0;i<n;i++)
         {
@@ -10,57 +9,54 @@ class Solution {
             {
                 if(grid[i][j]==1)
                 {
-                    mark++;
-                    int size=helper(grid,i,j,mark);
-                    res=Math.max(size,res);
+                    int size=dfs(grid,i,j,mark);
                     hm.put(mark,size);
+                    mark++;
                 }
             }
         }
+        int ans=0;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<n;j++)
             {
                 if(grid[i][j]==0)
                 {
-                    int g=1;
                     HashSet<Integer> hs=new HashSet<>();
-                    if(i>0)
+                    if(i>0 && grid[i-1][j]!=0)
                     {
                         hs.add(grid[i-1][j]);
                     }
-                    if(j>0)
+                    if(j>0 && grid[i][j-1]!=0)
                     {
                         hs.add(grid[i][j-1]);
                     }
-                    if(i<n-1)
+                    if(i<n-1 && grid[i+1][j]!=0)
                     {
                         hs.add(grid[i+1][j]);
                     }
-                    if(j<n-1)
+                    if(j<n-1 && grid[i][j+1]!=0)
                     {
                         hs.add(grid[i][j+1]);
                     }
-                    for(int f:hs)
+                    int res=0;
+                    for(int val:hs)
                     {
-                        if(f!=0)
-                        {
-                            g+=hm.get(f);
-                        }                        
+                        res+=hm.get(val);
                     }
-                    res=Math.max(res,g);
+                    ans=Math.max(ans,res+1);
                 }
             }
         }
-        return res;
+        return ans==0?(n*n):ans;
     }
-    public int helper(int grid[][],int i,int j,int mark)
+    public int dfs(int grid[][],int i,int j,int mark)
     {
-        if(i<0 || j<0 || i>=grid.length || j>=grid.length || grid[i][j]!=1)
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!=1)
         {
             return 0;
         }
         grid[i][j]=mark;
-        return 1+helper(grid,i-1,j,mark)+helper(grid,i,j-1,mark)+helper(grid,i+1,j,mark)+helper(grid,i,j+1,mark);
+        return 1+dfs(grid,i-1,j,mark)+dfs(grid,i,j-1,mark)+dfs(grid,i+1,j,mark)+dfs(grid,i,j+1,mark);
     }
 }
