528c86680b611f6a9ecc78e6799f25f2d4d0b018
Om Jaiswal <jaiswalom221@gmail.com>
2026-09-14 00:57:23 +0530

    Time: 42 ms (6.22%) | Memory: 52.6 MB (5.33%) - LeetSync

--- 85-maximal-rectangle/maximal-rectangle.java
+++ 85-maximal-rectangle/maximal-rectangle.java
@@ -2,73 +2,123 @@ class Solution {
     public int maximalRectangle(char[][] matrix) {
         int n=matrix.length;
         int m=matrix[0].length;
-        int height[]=new int[m];
-        int max_area=0;
+        int mat[][]=new int[n][m];
+        for(int j=0;j<m;j++)
+        {
+            mat[0][j]=(matrix[0][j]=='1')?1:0;
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                mat[j][i]=(matrix[j][i]=='0')?0:Integer.parseInt(matrix[j][i]+"")+mat[j-1][i];
+            }
+        }
+        int res=0;
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<m;j++)
-            {
-                height[j]=(matrix[i][j]=='0')?0:height[j]+1;
-            }
-            int ans=histogram(height);
-            max_area=Math.max(max_area,ans);
+            res=Math.max(res,solve(mat[i]));
         }
-        return max_area;
+        return res;
     }
-    public int histogram(int height[])
+    public int solve(int ar[])
     {
-        int n=height.length;
+        int n=ar.length;
         int pse[]=new int[n];
-        int nse[]=new int[n];
-        pse[0]=-1;
-        nse[n-1]=n;
         Stack<Integer> st1=new Stack<>();
-        Stack<Integer> st2=new Stack<>();
+        pse[0]=-1;
         st1.push(0);
-        st2.push(n-1);
         int i=1;
         while(i<n)
         {
-            while(st1.size()>0 && height[st1.peek()]>=height[i])
-            {
-                st1.pop();
-            }
-            if(st1.size()==0)
-            {
-                st1.push(i);
-                pse[i]=-1;
-            }
-            else
+            if(ar[i]>ar[st1.peek()])
             {
                 pse[i]=st1.peek();
                 st1.push(i);
             }
+            else
+            {
+                while(st1.size()!=0 && ar[i]<=ar[st1.peek()])
+                {
+                    st1.pop();
+                }
+                if(st1.size()==0)
+                {
+                    pse[i]=-1;
+                }
+                else
+                {
+                    pse[i]=st1.peek();
+                }
+                st1.push(i);
+            }
             i++;
         }
+        Stack<Integer> st2=new Stack<Integer>();
+        int nse[]=new int[n];
+        nse[n-1]=n;
+        st2.push(n-1);
         int j=n-2;
         while(j>=0)
         {
-            while(st2.size()>0 && height[st2.peek()]>=height[j])
-            {
-                st2.pop();
-            }
-            if(st2.size()==0)
-            {
-                st2.push(j);
-                nse[j]=n;
-            }
-            else
+            if(ar[st2.peek()]<ar[j])
             {
                 nse[j]=st2.peek();
                 st2.push(j);
             }
+            else
+            {
+                while(st2.size()!=0 && ar[j]<=ar[st2.peek()])
+                {
+                    st2.pop();
+                }
+                if(st2.size()==0)
+                {
+                    nse[j]=n;
+                }
+                else
+                {
+                    nse[j]=st2.peek();
+                }
+                st2.push(j);
+            }
             j--;
         }
-        int res=0;
-        for(int f=0;f<n;f++)
+        int ans=0;
+        for(int k=0;k<n;k++)
         {
-            res=Math.max(res,(nse[f]-pse[f]-1)*height[f]);
+            ans=Math.max(ans,(nse[k]-pse[k]-1)*ar[k]);
         }
-        return res;
+        return ans;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
