b6741c9848ddff5fb3d4f09ca8db06c3a11ab420
Om Jaiswal <jaiswalom221@gmail.com>
2026-09-20 20:30:10 +0530

    Time: 9 ms (19.13%) | Memory: 45.3 MB (45.27%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,35 +1,35 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
-       int n=prices.length;
-       int dp[][][]=new int[n][2][k+1];
-       for(int layer[][]:dp)
-       {
-        for(int i[]:layer)
+        int n=prices.length;
+        int dp[][][]=new int[n][2][k+1];
+        for(int layers[][]:dp)
         {
-            Arrays.fill(i,-1);
+            for(int i[]:layers)
+            {
+                Arrays.fill(i,-1);
+            }
         }
-       } 
-       return helper(prices,0,1,k,dp);
+        return helper(prices,0,1,k,dp);
     }
-    public int helper(int prices[],int idx,int buy,int cap,int [][][]dp)
+    public int helper(int prices[],int idx,int buy,int k,int dp[][][])
     {
-        if(idx==prices.length || cap==0)
+        if(idx==prices.length || k==0)
         {
             return 0;
         }
-        if(dp[idx][buy][cap]!=-1)
+        if(dp[idx][buy][k]!=-1)
         {
-            return dp[idx][buy][cap];
+            return dp[idx][buy][k];
         }
         int profit=0;
         if(buy==1)
         {
-            profit=Math.max(-prices[idx]+helper(prices,idx+1,0,cap,dp),helper(prices,idx+1,1,cap,dp));
+            profit=Math.max((-prices[idx]+helper(prices,idx+1,0,k,dp)),helper(prices,idx+1,1,k,dp));
         }
         else
         {
-            profit=Math.max(prices[idx]+helper(prices,idx+1,1,cap-1,dp),helper(prices,idx+1,0,cap,dp));
+            profit=Math.max((prices[idx]+helper(prices,idx+1,1,k-1,dp)),helper(prices,idx+1,0,k,dp));
         }
-        return dp[idx][buy][cap]=profit;
+        return dp[idx][buy][k]=profit;
     }
 }
